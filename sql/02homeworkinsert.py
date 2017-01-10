deeb72cd66a146ac35dbcd6b184574583bade815
Gan Lu <maburycn@hotmail.com>
2017-01-10 20:42:02 +0800

    modified:   sql/02homeworkinsert.py

--- sql/02homeworkinsert.py
+++ sql/02homeworkinsert.py
@@ -10,4 +10,6 @@ with sqlite3.connect("homework01.db") as conn:
     m.execute("INSERT INTO inventory VALUES('Fords','US',100)")
     m.execute("INSERT INTO inventory VALUES('Hondas','JP',200)")
     m.execute("INSERT INTO inventory VALUES('Hondas','JP',300)")
+    m.execute("INSERT INTO inventory VALUES('Audi','GR',400)")
+    m.execute("INSERT INTO inventory VALUES('MX5','CN',500)")
 
