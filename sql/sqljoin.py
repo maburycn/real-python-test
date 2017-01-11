2fafec73b2dd3562e1a06d02c97599a50c9dff00
Gan Lu <maburycn@hotmail.com>
2017-01-11 09:15:30 +0800

    Adding multi tables operatoin 	new file:   sql/joinresult.txt 	new file:   sql/sqljoin.py

---
+++ sql/sqljoin.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+import sqlite3
+
+
+with sqlite3.connect("new.db") as connection:
+    c = connection.cursor()
+
+    c.execute("""SELECT population.city,population.population,regions.region
+                 FROM population,regions WHERE population.city = regions.city""")
+
+
+    rows = c.fetchall()
+
+
+    for r in rows:
+        #print (r[0],r[1],r[2])
+        print ("City:" + r[0])
+        print ("Population:" + str(r[1]))
+        print ("Region:" + r[2])
+        print ("")
